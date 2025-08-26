1f889fc2153f618c0f3b149a4fad4a488946c985
cylee323071@gmail.com <cylee323071@gmail.com>
2025-08-26 14:55:27 +0900

    [Redux Toolkit] React 15일차 과제 제출

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import {ProVider} from "./redux/redux.jsx" 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+  </Provider>
   </BrowserRouter>
 );
